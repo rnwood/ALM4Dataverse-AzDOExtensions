21c7d7759d4545746a35304482a35f69ab2dd642
Rob Wood <rob@rnwood.co.uk>
2026-01-20 18:15:27 +0000

    Bundling

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const { resolve } = require("path");
+const fs = require("fs");
+
+// Find all task index.ts files
+const taskDirs = fs.readdirSync("src/tasks");
+const tasks = taskDirs
+  .map(dir => `src/tasks/${dir}/index.ts`)
+  .filter(file => fs.existsSync(file));
+
+module.exports = tasks.map((task) => ({
+  entry: `./${task}`,
+  target: "node",
+  externalsPresets: { node: true },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: [/node_modules/, /dist/],
+      },
+    ],
+  },
+  mode: "production",
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: task.replace(/\.ts$/, ".js").replace(/src[/\\]/, ""),
+    path: resolve(__dirname, "dist"),
+  },
+}));
